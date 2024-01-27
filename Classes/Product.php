0a91696c1eac568a6413c437c07aaaf27a47d201
UmbertoSerino <serumberto@gmail.com>
2024-01-27 20:15:06 +0100

    first commit

---
+++ Classes/Product.php
@@ -0,0 +1,49 @@
+<!-- Immaginare quali sono le classi necessarie per creare uno shop online con le seguenti caratteristiche:
+L'e-commerce vende prodotti per animali.
+
+I prodotti sono categorizzati, le categorie sono Cani o Gatti.
+I prodotti saranno oltre al cibo, anche giochi, cucce, etc.
+Stampiamo delle card contenenti i dettagli dei prodotti, come immagine, titolo, prezzo, icona della categoria
+ed il tipo di articolo che si sta visualizzando (prodotto, cibo, gioco, cuccia). -->
+
+<!-- classe padre, da qui dovremmo iniziare a modellare il prodotto, inserendo il prezzo, il formato del titolo, il formato del prezzo, l'immagine da inserire, descrizione... -->
+<!-- potremmo usare tre classi figlie a loro volta, una per cibo, un'altra per giochi e cucce ed un'altra per prodotti per animali  -->
+
+<?php
+require_once __DIR__ . '/Product.php';
+class Product
+{
+    protected $nameProduct;
+    protected $category;
+    protected $price;
+    protected $available;
+
+    /**
+     * fun construct Product
+     *
+     * @param string $_name
+     * @param Category $_category
+     * @param integer $_price
+     * @param boolean $_available
+     */
+    protected function __construct(string $_nameProduct, Category $_category, int $_price, bool $_available)
+    {
+        $this->nameProduct = $_nameProduct;
+        $this->category = $_category;
+        $this->price = $_price;
+        $this->available = $_available;
+    }
+
+    protected function setNameProduct($_nameProduct)
+    {
+        $this->nameProduct = ucfirst(strtolower($_nameProduct));
+    }
+
+    protected function setPrice($price)
+    {
+        if (is_string($price)) {
+            $this->price = intval($price);
+        }
+    }
+}
+?>
---
+++ index.php
@@ -0,0 +1,23 @@
+<!-- Immaginare quali sono le classi necessarie per creare uno shop online con le seguenti caratteristiche:
+L'e-commerce vende prodotti per animali.
+I prodotti sono categorizzati, le categorie sono Cani o Gatti.
+I prodotti saranno oltre al cibo, anche giochi, cucce, etc.
+Stampiamo delle card contenenti i dettagli dei prodotti, come immagine, titolo, prezzo, icona della categoria
+ed il tipo di articolo che si sta visualizzando (prodotto, cibo, gioco, cuccia). -->
+<?php
+require_once __DIR__ . '/Classes/Product.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pet Shop</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
